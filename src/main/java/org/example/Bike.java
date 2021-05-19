18dbb23d9a04d78259dd17b98c7efaa682b2b733
Elijah Busanga <elijahbus.ny@gmail.com>
2021-05-19 19:31:48 +0200

    ini,  spring dependency injection demo

---
+++ src/main/java/org/example/Bike.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Bike implements Vehicle
+{
+    public void drive()
+    {
+        System.out.println("Driving a bike");
+    }
+}
